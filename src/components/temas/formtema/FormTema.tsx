53c7336e4199dbeb66f80b9cf530bb038646f30f
LucasCapSilva <lcapelotto@gmail.com>
2025-01-22 07:56:52 -0300

    1.0

--- src/components/temas/formtema/FormTema.tsx
+++ src/components/temas/formtema/FormTema.tsx
@@ -1,109 +1,133 @@
 ﻿import { ChangeEvent, useContext, useEffect, useState } from "react";
+import { RotatingLines } from "react-loader-spinner";
 import { useNavigate, useParams } from "react-router-dom";
 import { AuthContext } from "../../../contexts/AuthContext";
 import Tema from "../../../models/Tema";
 import { atualizar, buscar, cadastrar } from "../../../services/Service";
 
 function FormTema() {
-  const navigate = useNavigate();
 
-  const [tema, setTema] = useState<Tema>({} as Tema);
+    const navigate = useNavigate();
 
-  const { usuario, handleLogout } = useContext(AuthContext);
-  const token = usuario.token;
+    const [tema, setTema] = useState<Tema>({} as Tema)
+    const [isLoading, setIsLoading] = useState<boolean>(false)
 
-  const { id } = useParams<{ id: string }>();
+    const { usuario, handleLogout } = useContext(AuthContext)
+    const token = usuario.token
 
-  async function buscarPorId(id: string) {
-    try {
-      await buscar(`/temas/${id}`, setTema, {
-        headers: { Authorization: token },
-      });
-    } catch (error: any) {
-      if (error.toString().includes("403")) {
-        handleLogout();
-      }
-    }
-  }
+    const { id } = useParams<{ id: string }>();
 
-  useEffect(() => {
-    if (token === "") {
-      alert("Você precisa estar logado!");
-      navigate("/");
-    }
-  }, [token]);
-
-  useEffect(() => {
-    if (id !== undefined) {
-      buscarPorId(id);
-    }
-  }, [id]);
-
-  function atualizarEstado(e: ChangeEvent<HTMLInputElement>) {
-    setTema({
-      ...tema,
-      [e.target.name]: e.target.value,
-    });
-  }
-
-  function retornar() {
-    navigate("/temas");
-  }
-
-  async function gerarNovoTema(e: ChangeEvent<HTMLFormElement>) {
-    e.preventDefault();
-
-    if (id !== undefined) {
-      try {
-        await atualizar(`/temas`, tema, setTema, {
-          headers: { Authorization: token },
-        });
-        alert("O Tema foi atualizado com sucesso!");
-      } catch (error: any) {
-        if (error.toString().includes("403")) {
-          handleLogout();
-        } else {
-          alert("Erro ao atualizar o tema.");
+    async function buscarPorId(id: string) {
+        try {
+            await buscar(`/temas/${id}`, setTema, {
+                headers: { Authorization: token }
+            })
+        } catch (error: any) {
+            if (error.toString().includes('403')) {
+                handleLogout()
+            }
         }
-      }
-    } else {
-      try {
-        await cadastrar(`/temas`, tema, setTema, {
-          headers: { Authorization: token },
-        });
-        alert("O Tema foi cadastrado com sucesso!");
-      } catch (error: any) {
-        if (error.toString().includes("403")) {
-          handleLogout();
-        } else {
-          alert("Erro ao cadastrar o tema.");
-        }
-      }
     }
 
-    retornar();
-  }
+    useEffect(() => {
+        if (token === '') {
+            alert('Você precisa estar logado!')
+            navigate('/')
+        }
+    }, [token])
 
-  return (
-    <div>
-      <h1>{id === undefined ? "Cadastrar Tema" : "Editar Tema"}</h1>
-      <form onSubmit={gerarNovoTema}>
-        <div>
-          <label htmlFor="descricao">Descrição do Tema</label>
-          <input
-            type="text"
-            placeholder="Descreva aqui seu tema"
-            name="descricao"
-            value={tema.descricao}
-            onChange={(e: ChangeEvent<HTMLInputElement>) => atualizarEstado(e)}
-          />
+    useEffect(() => {
+        if (id !== undefined) {
+            buscarPorId(id)
+        }
+    }, [id])
+
+    function atualizarEstado(e: ChangeEvent<HTMLInputElement>) {
+        setTema({
+            ...tema,
+            [e.target.name]: e.target.value
+        })
+    }
+
+    function retornar() {
+        navigate("/temas")
+    }
+
+    async function gerarNovoTema(e: ChangeEvent<HTMLFormElement>) {
+        e.preventDefault()
+        setIsLoading(true)
+
+        if (id !== undefined) {
+            try {
+                await atualizar(`/temas`, tema, setTema, {
+                    headers: { 'Authorization': token }
+                })
+                alert('O Tema foi atualizado com sucesso!')
+            } catch (error: any) {
+                if (error.toString().includes('403')) {
+                    handleLogout();
+                } else {
+                    alert('Erro ao atualizar o tema.')
+                }
+
+            }
+        } else {
+            try {
+                await cadastrar(`/temas`, tema, setTema, {
+                    headers: { 'Authorization': token }
+                })
+                alert('O Tema foi cadastrado com sucesso!')
+            } catch (error: any) {
+                if (error.toString().includes('403')) {
+                    handleLogout();
+                } else {
+                    alert('Erro ao cadastrar o tema.')
+                }
+
+            }
+        }
+
+        setIsLoading(false)
+        retornar()
+    }
+
+    return (
+        <div className="container flex flex-col items-center justify-center mx-auto">
+            <h1 className="text-4xl text-center my-8">
+                {id === undefined ? 'Cadastrar Tema' : 'Editar Tema'}
+            </h1>
+
+            <form className="w-1/2 flex flex-col gap-4" onSubmit={gerarNovoTema}>
+                <div className="flex flex-col gap-2">
+                    <label htmlFor="descricao">Descrição do Tema</label>
+                    <input
+                        type="text"
+                        placeholder="Descreva aqui seu tema"
+                        name='descricao'
+                        className="border-2 border-slate-700 rounded p-2"
+                        value={tema.descricao}
+                        onChange={(e: ChangeEvent<HTMLInputElement>) => atualizarEstado(e)}
+                    />
+                </div>
+                <button
+                    className="rounded text-slate-100 bg-indigo-400 
+                               hover:bg-indigo-800 w-1/2 py-2 mx-auto flex justify-center"
+                    type="submit">
+                    {isLoading ?
+                        <RotatingLines
+                            strokeColor="white"
+                            strokeWidth="5"
+                            animationDuration="0.75"
+                            width="24"
+                            visible={true}
+                        /> :
+                        <span>{id === undefined ? 'Cadastrar' : 'Atualizar'}</span>
+
+                    }
+                </button>
+            </form>
         </div>
-        <button type="submit">
-          <span>{id === undefined ? "Cadastrar" : "Atualizar"}</span>
-        </button>
-      </form>
-    </div>
-  );
+    );
 }
 
 export default FormTema;
--- src/components/temas/listatemas/ListaTemas.tsx
+++ src/components/temas/listatemas/ListaTemas.tsx
@@ -1,4 +1,5 @@
 ﻿import { useContext, useEffect, useState } from "react";
+import { DNA } from "react-loader-spinner";
 import { useNavigate } from "react-router-dom";
 import { AuthContext } from "../../../contexts/AuthContext";
 import Tema from "../../../models/Tema";
@@ -6,44 +7,60 @@ import CardTema from "../cardtemas/CardTemas";
 import { buscar } from "../../../services/Service";
 
 function ListaTemas() {
-  const navigate = useNavigate();
-  const [temas, setTemas] = useState<Tema[]>([]);
-  const { usuario, handleLogout } = useContext(AuthContext);
-  const token = usuario.token;
 
-  async function buscarTemas() {
-    try {
-      await buscar("/temas", setTemas, {
-        headers: { Authorization: token },
-      });
-    } catch (error: any) {
-      if (error.toString().includes("403")) {
-        handleLogout();
-      }
+    const navigate = useNavigate();
+
+    const [temas, setTemas] = useState<Tema[]>([])
+
+    const { usuario, handleLogout } = useContext(AuthContext)
+    const token = usuario.token
+
+    async function buscarTemas() {
+        try {
+            await buscar('/temas', setTemas, {
+                headers: { Authorization: token }
+            })
+        } catch (error: any) {
+            if (error.toString().includes('403')) {
+                handleLogout()
+            }
+        }
     }
-  }
 
-  useEffect(() => {
-    if (token === "") {
-      alert("Você precisa estar logado!");
-      navigate("/");
-    }
-  }, [token]);
+    useEffect(() => {
+        if (token === '') {
+            alert('Você precisa estar logado!')
+            navigate('/')
+        }
+    }, [token])
 
-  useEffect(() => {
-    buscarTemas();
-  }, [temas.length]);
+    useEffect(() => {
+        buscarTemas()
+    }, [temas.length])
 
-  return (
-    <>
-      
-      <div>
-        {temas.map((tema) => (
-          <CardTema key={tema.id} tema={tema} />
-        ))}
-      </div>
-    </>
-  );
+    return (
+        <>
+            {temas.length === 0 && (
+                <DNA
+                    visible={true}
+                    height="200"
+                    width="200"
+                    ariaLabel="dna-loading"
+                    wrapperStyle={{}}
+                    wrapperClass="dna-wrapper mx-auto"
+                />
+            )}
+            <div className="flex justify-center w-full my-4">
+                <div className="container flex flex-col">
+                    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
+                        {temas.map((tema) => (
+                            <CardTema key={tema.id} tema={tema} />
+                        ))}
+                    </div>
+                </div>
+            </div>
+        </>
+    )
 }
 
 export default ListaTemas;
